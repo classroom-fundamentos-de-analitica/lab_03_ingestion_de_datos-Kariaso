28dc62c282872eaaa095a9104982913f36d36e96
Kariaso <159803865+Kariaso@users.noreply.github.com>
2024-08-26 09:09:40 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,30 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
+    with open('clusters_report.txt') as report:
+        rows = report.readlines()
+    rows = rows[4:]
+    df = pd.DataFrame(columns=["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave","principales_palabras_clave"]) 
+    for row in rows:
+        if re.match('^ +[0-9]+ +', row):
+            p_claves=""
+            cluster, num_pc, porcen, *pc = row.split()
+            pc.pop(0) 
+            pc = ' '.join(pc) 
+            p_claves += pc
+            df.loc[cluster]=[int(cluster),int(num_pc),float(porcen.replace(',','.')),p_claves]
+        elif re.match('^ +[a-z]', row):
+            pc = row.split()
+            pc = ' '.join(pc) 
+            p_claves += ' ' + pc 
+            df.at[cluster, 'principales_palabras_clave'] = p_claves
+        
+        elif re.match('^\n', row) or re.match('^ +$', row):
+            df.at[cluster, 'principales_palabras_clave'] = df.at[cluster, 'principales_palabras_clave'].replace('.', '')
+        
+    df.reset_index(drop=True, inplace=True)
 
     return df
